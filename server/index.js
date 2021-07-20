e496643da238d9ed170eac928a730cc93f99e08c
gerhard.molin <gmolin@student.hive.fi>
2021-07-20 16:07:40 +0300

    settig up socket-io backend

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const http = require('http');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
+
