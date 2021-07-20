4f1e07bed0281c6b7a07fc7378f211a68a3308f3
gerhard.molin <gmolin@student.hive.fi>
2021-07-20 16:37:00 +0300

    Join component done

---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+import QueryString from 'qs';
+import io from 'socket.io-client';
+
+import './Chat.css';
+
+const Chat = () => {
+
+    useEffect(() => {
+
+    }, []);
+
+    return (
+        <div>Chat</div>
+    )
+};
+
+export default Chat;
--- server/index.js
+++ server/index.js
@@ -5,8 +5,19 @@ const socketio = require('socket.io');
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+const router = require('./controllers/router');
 
 const PORT = process.env.PORT || 5000;
+
+io.on('connection', (socket) => {
+    console.log(`We have a new connection`);
+    
+    socket.on('disconnect', () => {
+        console.log('User has left');
+    });
+});
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
 
