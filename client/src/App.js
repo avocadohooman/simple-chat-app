3c418e1bcd76a2a5c015064a6c6550d661816c2b
gerhard.molin <gmolin@student.hive.fi>
2021-07-20 15:58:26 +0300

    initial client components

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-const App = () => {
-  return ( 
-    <div>Hello World</div>
-  )
-}
+import Join from './components/Join';
+import Chat from './components/Chat';
+
+const App = () => (
+  <Router>
+
+    <Route path="/" exact component={Join} />
+    <Route path="/chat" component={Chat} />
+
+  </Router>
+);
 
 export default App;
