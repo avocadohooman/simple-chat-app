b5179df1b803808387148a7113865860e076b527
gerhard.molin <gmolin@student.hive.fi>
2021-07-21 16:22:32 +0300

    Input component added

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -3,6 +3,7 @@ import queryString from 'query-string';
 import io from 'socket.io-client';
 import './Chat.css';
 import InfoBar from '../InfoBar/InfoBar';
+import Input from '../Input/Input';
 
 let socket;
 
@@ -53,11 +54,7 @@ const Chat = ({location}) => {
         <div className="outerContainer">
             <div className="container">
                 <InfoBar room={room}/>
-                <input 
-                    value={message} 
-                    onChange={(event) => setMessage(event.target.value)} 
-                    onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
-                />
+                <Input message={message} sendMessage={sendMessage} setMessage={setMessage}/>
             </div>
         </div>
     )
---
+++ client/src/components/Input/Input.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Input.css';
+
+const Input = (({message, sendMessage, setMessage}) => {
+    return (
+        <form className="form">
+            <input 
+                className="input"
+                placeholder="Type a message"
+                type="text"
+                value={message} 
+                onChange={(event) => setMessage(event.target.value)} 
+                onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
+            />
+            <button className="sendButton" onClick={event => sendMessage(event)}>Send</button>
+        </form>
+    )
+});
+
+export default Input;
