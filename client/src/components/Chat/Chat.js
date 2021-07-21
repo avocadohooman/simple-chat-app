6fbfc728e903f3e30cfe2a3b6ba22c2d20799823
gerhard.molin <gmolin@student.hive.fi>
2021-07-21 15:32:44 +0300

    adding userServices for server and basic socket functionalities

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 import './Chat.css';
@@ -10,15 +10,31 @@ const Chat = ({location}) => {
 
     const [name, setName] = useState('');
     const [room, setRoom] = useState('');
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+
     const ENDPOINT = 'localhost:5000';
+
     useEffect(() => {
         const {name, room} = queryString.parse(location.search);
         socket = io(ENDPOINT);
         setName(name);
         setRoom(room);
-        socket.emit()
+        socket.emit('join', { name: name, room: room });
+        
+        return () => {
+            socket.emit('disconnect');
+            socket.off();
+        }
     }, [ENDPOINT, location.search]);
 
+    useEffect(() => {
+        socket.on('message', (message) => {
+            setMessages([...messages, message]);
+        });
+    }, [messages]);
+
+    
     return (
         <div>Chat</div>
     )
--- server/index.js
+++ server/index.js
@@ -1,23 +1,40 @@
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
-
 const app = express();
 const server = http.createServer(app);
+const cors = require('cors');
 corsOptions={
     cors: true,
     origins:["http://localhost:3000"],
 }
 const io = socketio(server, corsOptions);
 const router = require('./controllers/router');
-const cors = require('cors');
-
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./services/userService');
 
 const PORT = process.env.PORT || 5000;
 
 io.on('connection', (socket) => {
-    console.log(`We have a new connection`);
     
+    socket.on('join', ({name, room}, callback) => {
+        const { error, user } = addUser({ id: socket.id, name, room });
+        if (error) {
+            return callback(error);
+        }
+        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the room: ${user.room}` });
+        socket.broadcast.to(user.room).emit('message',{ user: 'admin', text: `${user.name} has joined` });
+        socket.join(user.room);
+        
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', {user: user.name, text: message});
+        callback();
+    });
+
     socket.on('disconnect', () => {
         console.log('User has left');
     });
